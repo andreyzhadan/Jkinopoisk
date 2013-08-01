36abd9429627a85d8aeaef61611407161b5078ec
andreyzhadan <azhadan@griddynamics.com>
2013-08-01 16:32:37 +0300

    Initial commit

---
+++ src/main/java/com.zhadan/bean/User.java
@@ -0,0 +1,39 @@
+package com.zhadan.bean;
+
+/**
+ * Created by azhadan on 7/30/13.
+ */
+public class User {
+    private String login;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String login, String password) {
+
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return this.getLogin() + " / " + this.getPassword();
+    }
+}
---
+++ src/main/webapp/jkinopoisk/index.jsp
@@ -0,0 +1,12 @@
+<%--
+  Created by azhadan on 7/29/13.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index page</title>
+</head>
+<body>
+<h2>Index page</h2>
+</body>
+</html>
